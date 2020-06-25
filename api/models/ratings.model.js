8db99f779f7464b749d1f5a63b44c0b7dae984b5
ratoledo <rtoledocastellano@gmail.com>
2020-06-25 21:56:17 +0100

    Added rating model

---
+++ api/models/ratings.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const ratingSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Types.ObjectId,
+    required: [true, 'User is required']
+  },
+  pt_general: {
+    type: Number,
+    required: [true, 'Score_general is required']
+  },
+  pt_price: {
+    type: Number,
+    required: [true, 'Score_price is required']
+  },
+  pt_quality: {
+    type: Number,
+    required: [true, 'Score_quality is required']
+  },
+  comment: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+})
+
+const ratingModel = mongoose.model('rating', ratingSchema)
+module.exports = ratingModel
