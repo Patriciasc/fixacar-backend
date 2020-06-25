aab302a2758a238912ca1bbf08b1c13b5bf2d79d
ratoledo <rtoledocastellano@gmail.com>
2020-06-25 22:11:33 +0100

    Added Budget Controller

---
+++ api/controllers/budget.controller.js
@@ -0,0 +1,19 @@
+// const budgetModel = require('../models/budgets.model')
+
+module.exports = {
+  getUserbudgets,
+  updateUserBudget,
+  createBudget
+}
+
+function getUserbudgets (req, res) {
+  console.log('budget: getUserbudgets')
+}
+
+function updateUserBudget (req, res) {
+  console.log('budget: updateUserBudget')
+}
+
+function createBudget (req, res) {
+  console.log('budget: createBudget')
+}
