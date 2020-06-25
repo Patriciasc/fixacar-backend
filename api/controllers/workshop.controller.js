f89cabd6f6455403e3b32512dd2b514fb57ae676
ratoledo <rtoledocastellano@gmail.com>
2020-06-25 22:08:03 +0100

    Added Workshop Controller

---
+++ api/controllers/workshop.controller.js
@@ -0,0 +1,24 @@
+// const WorkshopModel = require('../models/workshops.model')
+
+module.exports = {
+  getAllWorkshops,
+  getWorkshopById,
+  getRatings,
+  addRating
+}
+
+function getAllWorkshops (req, res) {
+  console.log('Workshop: getAllWorkshops')
+}
+
+function getWorkshopById (req, res) {
+  console.log('Workshop: getWorkshopById')
+}
+
+function getRatings (req, res) {
+  console.log('Workshop: getRatings')
+}
+
+function addRating (req, res) {
+  console.log('Workshop: AddRating')
+}
