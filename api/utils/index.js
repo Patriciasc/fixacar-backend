1300f54ad11bddbab994a431c01ca010249f03d5
Patricia Santana Cruz <patriciasantanacruz@gmail.com>
2020-06-25 20:32:18 +0100

    Added Auth middleware

---
+++ api/utils/index.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken')
+const UserModel = require('../models/users.model')
+
+// Authenticate Middleware
+function authUser (req, res, next) {
+  if (!req.headers.token) {
+    res.status(403).json({ error: 'No Token found' })
+  } else {
+    try {
+      const decodedToken = jwt.verify(req.headers.token, process.env.SECRET)
+
+      UserModel
+        .findOne({ email: decodedToken.email })
+        .then(user => {
+          res.locals.user = user
+          next()
+        })
+    } catch (error) {
+      res.status(403).json({ error: `Token not valid + ${error}` })
+    }
+  }
+}
+
+// Return HTTP error with details in JSON
+function handleError (err, res) {
+  return res.status(400).json(err)
+}
+
+module.exports = {
+  authUser,
+  handleError
+}
